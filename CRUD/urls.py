72403ca8bfc3c0ab0780175bcd582fc3f1664315
shahadat1010 <19101071@uap-bd.edu>
2023-02-20 17:39:01 +0600

    This is a Employee app
    This is a Employee app

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -21,4 +21,8 @@ from .import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.INDEX,name="home"),
-]
+    path('add',views.ADD,name='add'),
+    path('edit',views.EDIT,name='edit'),
+    path('update/<str:id>',views.UPDATE,name='update'),
+    path('delete/<str:id>',views.DELETE,name='delete'),
+    ]
